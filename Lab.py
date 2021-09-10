f753c94c678cc0d669f1e49d491404038f22ebcf
Jed Rembold <jedediah.r@gmail.com>
2021-09-10 00:07:16 -0700

    Add initial contents

---
+++ Lab.py
@@ -0,0 +1,51 @@
+"""
+A lab in which triangular numbers are being computed.
+"""
+
+# For convenience, it is still a good idea to put your code in a
+# function. So I will define a basic function below which you can
+# place your code inside.
+
+def triangle_number():
+    """
+    Computes the nth triangle number, where n is define in the below variable.
+
+    Alternatively, may also print out the percentage of the first 1000 triangle numbers
+    which are odd.
+    """
+    
+    n = 2
+
+    # And now add your code below! Make sure it is indented to be part of the function!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Unlike Karel, Python does not know when you run a script what function you necessarily
+# want to run, and it does not assume the first. The below boilerplate takes care of calling
+# the triangle_number() function automatically whenever your program is run.
+if __name__ == '__main__':
+    triangle_number()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Lab_TriangleNumbers
-A lab focusing on loops, variable assignment, and arithmetic to compute triangular numbers.
+# Lab 4: Triangle Numbers
+This pair programming lab focuses on using loops, variable assignment, and basic arithmetic to compute various triangular numbers. See the guide for the full instructions.
+
+I will just be checking people off as them complete the lab today. You only need to submit back to Github here if you want an online record of what you did (which isn't a terrible idea!).
